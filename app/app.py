433a0628e7d7b9dacc06d4afc20d0c653b69fc22
n1C01451997 <canonalejandro17@gmail.com>
2024-02-28 09:48:43 -0500

    register

--- app/app.py
+++ app/app.py
@@ -1,29 +1,39 @@
-from flask import Flask,render_template
+from flask import *
 import mysql.connector
 
-#Crear instancia 
-app = Flask (__name__)
+# Crear instancia
+app = Flask(__name__)
 
-#configurar la conexion 
+# Configurar la conexión
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="agenda2024" 
+    database="agenda2024"
 )
 
 cursor = db.cursor()
 
-@app.route('/') #crear ruta
+@app.route('/')  # Crear ruta
 def index():
     return render_template('index.html')
 
-@app.route('/Registrar')
-def registrar():
-    return render_template('Registrar.html')
-#ejecutar app 
-if __name__ == '__main__':
-    app.add_url_rule('/',view_func=index)
-    app.run(debug=True,port=5005) #debug para que me salgan los errores en consola 
+@app.route('/Registrar', methods=['POST'])
+def registrar_usuario():
+    Nombres = request.form['nombre']
+    Apellidos = request.form['apellido']
+    email = request.form['email']
+    Direccion = request.form['direccion']
+    Telefono = request.form['telefono']
+    Usuario = request.form['usuario']
+    Password = request.form['password']
+    
+    # Insertar datos a la tabla de mysql
+    cursor.execute("INSERT INTO personas(nombrep, apellidop, emailp, dirp, telp, usup, passp) VALUES (%s, %s, %s, %s, %s, %s, %s)", (Nombres, Apellidos, email, Direccion, Telefono, Usuario, Password))
+    db.commit()
+    
+    return redirect(url_for('index'))
 
-#crear ruta 
+# Ejecutar app
+if __name__ == '__main__':
+    app.run(debug=True, port=5005)  # Debug para que salgan los errores en consola
