21031b81af7b52073a42bffd7dcfc94edb7dd7c2
KerynM96 <mkerynmayerly@gmail.com>
2024-03-04 09:39:47 -0500

    third

--- app/app.py
+++ app/app.py
@@ -15,8 +15,12 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 @app.route('/')  # Crear ruta
-def index():
-    return render_template('index.html')
+def lista():
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM personas')
+    usuario = cursor.fetchall()
+
+    return render_template('index.html' ,usuario=usuario)
 
 @app.route('/Registrar', methods=['GET', 'POST'])
 def registrar_usuario():
@@ -30,7 +34,7 @@ def registrar_usuario():
        Password = request.form.get('password')
     
         # Insertar datos a la tabla de mysql
-       cursor.execute("INSERT INTO personas(nombrep,apellidop,emailp,dirp,telp,usup,passp) VALUES (%s,%s,%s,%s,%s,%s,%s)", (Nombres,Apellidos,email,Direccion,Telefono,Usuario,Password))
+       cursor.execute("INSERT INTO personas(nombrep,apellidop,emailp,dirp,telp,usup,passp) VALUES (%s,%s,%s,%s,%s,%s,%s)",(Nombres,Apellidos,email,Direccion,Telefono,Usuario,Password))
        db.commit()
 
             
@@ -39,4 +43,6 @@ def registrar_usuario():
 
 # Ejecutar app
 if __name__ == '__main__':
+    app.add_url_rule('/',view_func=lista)
     app.run(debug=True, port=5005)  # Debug para que salgan los errores en consola
+
