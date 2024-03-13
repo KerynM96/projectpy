6d3851a231ae741d5b4f7389b1f0cf621204d93d
KerynM96 <113958927+KerynM96@users.noreply.github.com>
2024-03-13 09:38:29 -0500

    eight

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify,session
 import mysql.connector
-import bcrypt
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # Crear instancia
@@ -15,6 +14,7 @@ db = mysql.connector.connect(
 )
 
 cursor = db.cursor()
+
 @app.route('/password/<contraencrip>')
 def encriptarcontra(contraencrip):
     #generar un hash de la contraseña
@@ -22,7 +22,8 @@ def encriptarcontra(contraencrip):
     encriptar= generate_password_hash(contraencrip)
     value= check_password_hash(encriptar,contraencrip)
     
-    return "Encriptado:{0} | coincide:{1}".format(encriptar,value)
+    #return "Encriptado:{0} | coincide:{1}".format(encriptar,value)
+    return value
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -31,18 +32,23 @@ def login():
         password = request.form.get('txtcontrasena')
         
         cursor =db.cursor()
-        cursor.execute("SELECT usup,pass FROM persona WHERE usup = %s",(username,))
+        cursor.execute("SELECT usup, pass FROM persona WHERE usup = %s",(username,))
         usuarios = cursor.fetchone()
         
-        if usuarios or encriptarcontra(password) == usuarios[1]:
+        if usuarios or check_password_hash(usuarios[1], password):
             session['usuario'] = username
             return redirect(url_for('lista'))
         else:
-            error = 'credenciales invalidas. por favor intente de nuevo'
-            return render_template('login.html', error=error)
-        
+            print("Credenciales invalidas")
+            print("Credenciales invalidas. Por favor intentelo de nuevo")
+            return render_template('login.html')
     return render_template('login.html')
-        
+
+@app.route('/logout')
+def logout():
+          #Eliminar el usuario de la sesión
+          session.pop['usuario', None]
+          return redirect/url_for(('login'))
 
 @app.route('/')  # Crear ruta
 def lista():
@@ -63,15 +69,27 @@ def registrar_usuario():
        Usuario = request.form.get('usuario')
        Password = request.form.get('password')
        
-       encryptpassword = encriptarcontra(Password)
+       #Encriptar la contraseña
+       encryptpassword = generate_password_hash(Password)
        
+       cursor=db.cursor()
+       cursor.execute( "SELECT * FROM persona WHERE usup = %s OR email = %s", (Usuario, email))
+       existing_user = cursor.fetchone()
+       
+       if existing_user:
+           print('El usuario o correo ya esta registrado.')
+           return render_template('Registrar.html')
     
         # Insertar datos a la tabla de mysql
-       cursor.execute("INSERT INTO persona(nombrep, apellidop, email, dirp, tel, usup, pass) VALUES (%s, %s, %s, %s, %s, %s, %s)", (Nombres, Apellidos, email, Direccion, Telefono, Usuario, encryptpassword))
+      
+       cursor.execute("INSERT INTO persona(nombrep, apellidop, email, dirp, tel, usup, pass) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+        (Nombres,Apellidos,email,Direccion, Telefono, Usuario, encryptpassword))
        db.commit()
 
             
-       return redirect(url_for('lista'))  # Redirigir a la página principal
+       return redirect(url_for('login'))  # Redirigir a la página principal
+    #return redirect(url_for('Registrar usuario'))
+
     return render_template("Registrar.html")
 
 @app.route('/editar/<int:id>',methods=['GET', 'POST'])
@@ -100,17 +118,10 @@ def editar_usuario(id):
 
 @app.route("/eliminar/<int:id>", methods=['GET', 'POST'])
 def eliminar_usuario(id):
-    cursor = db.cursor()
-    if request.method == 'POST':
-        nombreper = request.form.get('nombreper')
-        apellidoper = request.form.get('apellidoper')
-        emailper = request.form.get('emailper')
-        dirper = request.form.get('dirper')
-        telper = request.form.get('telper')
-        usuper = request.form.get('usuper')
-        passper = request.form.get('passper')
-        
+    cursor = db.cursor()    
     cursor.execute('DELETE FROM persona WHERE polper=%s', (id,))
+    db.commit()
+    cursor.close()
 
     return redirect(url_for('lista'))
         
