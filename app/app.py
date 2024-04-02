c1bc82b8f48f0c0155b5a4b224edc73b4819dc33
KerynM96 <113958927+KerynM96@users.noreply.github.com>
2024-04-02 09:39:02 -0500

    uno

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify,session
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import base64
 # Crear instancia
 app = Flask(__name__)
 app.secret_key = '789456123'
@@ -39,6 +39,7 @@ def login():
         if usuarios or check_password_hash(usuarios['pass'], password):
             session['usuario'] = username ['usup']
             session['rol'] = ['roles']
+            
             if usuarios['roles'] == 'administrator':
                 return redirect(url_for('lista'))
             else:
@@ -53,6 +54,7 @@ def login():
 def logout():
     # Eliminar el usuario de la sesión
     session.pop('usuario', None)
+    print("La sesión se ha cerrado")
     return redirect(url_for('login'))
 
 
@@ -131,8 +133,8 @@ def eliminar_usuario(id):
 
     return redirect(url_for('lista'))
 
-@app.route('/eliminar_cancion/<int:id>',methods=['GET', 'POST'])
-def cancelar(id):
+@app.route('/registro/<int:id>',methods=['GET', 'POST'])
+def registro_cancion():
     cursor = db.cursor()
     if request.method == 'POST':
         idcan = request.form.get('id_can')
@@ -142,28 +144,69 @@ def cancelar(id):
         precioc = request.form.get('precio')
         duracionc = request.form.get('duracion')
         lanzamientoc = request.form.get('lanzamiento')
-        imgc = request.form.get('img')
+        imgc = request.files['img']
+        
+        imagenblob = imgc.read()
+        
+        cursor = db.cursor()
 
-        sql = "UPDATE canciones SET  titulo=%s, artista=%s, genero=%s, precio=%s, duracion=%s, lanzamiento=%s, img=%s WHERE id_can=%s,"
-        cursor.execute(sql,(tituloc, artistac, generoc, precioc, duracionc,lanzamientoc,imgc,id,))
+        
+        cursor.execute("INSERT INTO canciones (titulo, artista, genero, precio, duracion, lanzamiento,img) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+                       (idcan,tituloc, artistac, generoc, precioc, duracionc,lanzamientoc,imgc,imagenblob))
+        db.commit()
+        return redirect(url_for('registro_cancion'))
+    return render_template("Registro.html")
+
+#Enlaazar-actualizar 
+@app.route('/actualizar/<int:id>',methods=['GET', 'POST'])   
+def editar_cancion(id): 
+    cursor = db.cursor()
+    if request.method == 'POST':
+        tituloc = request.form.get('titulo')
+        artistac = request.form.get('artista')
+        generoc = request.form.get('genero')
+        precioc = request.form.get('precio')
+        duracionc = request.form.get('duracion')
+        lanzamientoc = request.form.get('lanzamiento')
+        
+        sql = "UPDATE canciones SET titulo=%s, artista=%s, genero=%s, precio=%s,duracion=%s,lanzamiento=%s, where id_can=%s"
+        cursor.execute(sql,(tituloc, artistac, generoc, precioc, duracionc,lanzamientoc,id,))
         db.commit()
         return redirect(url_for('lista'))
-    
-    else: 
+     
+    else:
         cursor = db.cursor()
-        cursor.execute('SELECT * FROM persona WHERE id_can=%s' ,(id,))
+        cursor.execute('SELECT * FROM canciones WHERE id_canciones=%s' ,(id,))
         data = cursor.fetchall()
 
-        return render_template('actualizar.html', persona=data[0])
+        return render_template('actualizar.html', canciones=data[0])
 
-@app.route("/eliminar/<int:id>", methods=['GET', 'POST'])
-def eliminar_cancion(id):
-    cursor = db.cursor()    
-    cursor.execute('DELETE FROM persona WHERE polper=%s', (id,))
-    db.commit()
-    cursor.close()
+@app.route('/mostrarcanciones' , methods=["GET" , "POST"])
+def mostrar_canciones():
+    cursor = db.cursor()
+    cursor.execute('SELECT titulo,artista,genero,precio,duracion,lanzamiento,img FROM canciones')
+    canciones = cursor.fetchall()
 
-    return redirect(url_for('lista'))       
+    if canciones:
+     cancioneslist = []
+     
+     for cancion in canciones:
+        imagen = base64.b64encode(cancion[6]).decode('utf-8')
+        cancioneslist.append ({
+            'titulo':cancion[0],
+            'artista':cancion[1],
+            'genero':cancion[2],
+            'precio':cancion[3],
+            'duracion':cancion[4],
+            'lanzamiento':cancion[5],
+            'img':imagen   
+        })
+        
+        return render_template("canciones.html", cancion=cancioneslist)
+    else: 
+        return print ("canciones.html")   
+    
+    
 # Ejecutar app
 if __name__ == '__main__':
     app.add_url_rule('/',view_func=lista)
